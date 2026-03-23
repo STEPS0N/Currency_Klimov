31666d2c2137f4eb97061b3e256aa597aadbf4f9
STEPS0N <stepan.klimoff@mail.ru>
2026-03-23 21:00:26 +0500

    Разработанный проект на оценку удовлетворительно. Климов

--- app/src/main/java/com/example/currency_klimov/MainActivity.java
+++ app/src/main/java/com/example/currency_klimov/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //  Метод для вывода ошибок в всплывающее диалоговое окно
     public void AlterDialogs(String title, String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(title)
@@ -47,21 +48,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Consider(View view){
+        //  Получение id елементов в activity_main
         EditText course = findViewById(R.id.etRate);
         EditText count = findViewById(R.id.etSum);
-        Switch dollar = findViewById(R.id.switch1);
+        Switch dollar = findViewById(R.id.spinnerCurrency);
         TextView tv = findViewById(R.id.courseText);
 
+        //  Если поле курса пустое, то вывести сообщение об ошибке
         if (course.getText().length() > 0) {
+            //  Если поле суммы пустое, то вывести сообщение об ошибке
             if (count.getText().length() > 0) {
+                // Получаем данные из полей ввода
                 float f_course = Float.parseFloat(String.valueOf(course.getText()));
                 float f_count = Float.parseFloat(String.valueOf(count.getText()));
 
+                // Конвертация в рубли или доллары
                 float composition = 0;
                 if (dollar.isChecked() == true){
+                    // Конвертация: валюта - рубли
                     composition = f_course * f_count;
                     tv.setText(composition + " р.");
                 } else {
+                    // Конвертация: рубли - валюта
                     composition = f_count / f_course;
                     tv.setText(composition + " $.");
                 }
@@ -70,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void URL(View view){
+        // Переход по ссылке при нажатии по тексту
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sberbank.ru/ru/quotes/currencies"));
         startActivity(intent);
     }
